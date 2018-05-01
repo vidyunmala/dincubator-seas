0e3eea3809ad41ba97725e204ff3815cfe466324
vidyunmala <vidyunmala@gmail.com>
2018-05-01 17:22:21 +0200

    Add files via upload

--- dincubator-seas/src/App.js
+++ dincubator-seas/src/App.js
@@ -8,12 +8,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Season Ahead</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         In this webpage we will show the multiple uses of seasonal forecasts. How to use them, what are the quality checks behind the datasets. User needs that can be resolved through the same. We look forward to interacting with you soon.  
         </p>
+
+
+        < h2 className="App description"> STAY TUNED!! </h2>
+
       </div>
+  
+
+      
     );
   }
 }
